58e77866182db4c3e6b12598ad9f2748cad2c134
CheBarash <chebarashek@gmail.com>
2023-11-16 22:26:23 +0500

    Help command

--- index.js
+++ index.js
@@ -19,6 +19,14 @@ bot.use(async (_ctx, next) => {
 bot.start((ctx) =>
   ctx.reply(`Send completely anonymous gossip to @puanonymous`)
 );
+
+bot.help((ctx) =>
+  ctx.reply(
+    `<b>A few tips to make your bot experience better:</b>\n\n<b>1.</b> If you want to reply to someone from the channel, you can simply select <i>"reply in another chat"</i> then select me and reply to him.\n\n<b>2.</b> This bot is completely anonymous and you can verify by opening the <a href="https://github.com/chebarash/anonymous">source code</a>.\n\n<b>3.</b> If you still have questions or suggestions, just contact <a href="http://t.me/chbrsh">my developer</a>.`,
+    { parse_mode: `HTML` }
+  )
+);
+
 bot.on(message(), (ctx) => ctx.copyMessage(parseInt(CHANNEL)));
 
 (async () => {
